28ec789c0a6417d2fb949ec4ca9e7eeec48e4d33
jasonm23 <jasonm23@gmail.com>
2015-11-21 22:31:33 +0800

    Rename Phoenix-config.litcoffee to .phoenix.js (use the coffee shebang)

--- .phoenix.js
+++ .phoenix.js
@@ -1,31 +1,40 @@
-# Phoenix.app config
+#!/usr/local/bin/coffee -bcpl
+# -*- mode: litcoffee -*- <= For Emacs to switch mode to Literate CoffeeScript
 
-## Newsflash: Moved to https://github.com/jasonm23/Phoenix-config/
+# Phoenix.app config
 
 ## Prologue
 
 This is a nice, fairly comprehensive, relatively self-documenting,
-configuration for [Phoenix.app](https://github.com/jasonm23/Phoenix),
+configuration for [Phoenix 2](https://github.com/kasper/phoenix/tree/2.0),
 a lightweight scriptable OS X window manager.
 
 ## [Jump straight to the bindings](#bindings)
 
 ## Usage
 
-Install Phoenix.app, and convert this file (`Phoenix-config.litcoffee`) to
-plain JS, for use with Phoenix.app using:
+Build Phoenix.app from [kasper/phoenix 2.0 branch](https://github.com/kasper/phoenix/tree/2.0)
+
+You will need XCode command line tools installed.
 
 ```bash
-coffee --bare --literate --compile Phoenix-config.litcoffee
-mv Phoenix-config.js ~/.phoenix.js
+git clone https://github.com/kasper/phoenix
+cd phoenix
+git checkout 2.0
+xcodebuild
+open build/Release
 ```
 
-That's if you want to type all that, and be in full awareness of what
-is being done. Alternatively, run:
+Then Copy this file to `~/.phoenix.js`
 
-```bash
-make
-```
+Now drab and drop Phoenix into `/Applications/` and start it. You'll
+need to enable the security / accessibility settings (a pop up will
+tell you this on first start.)
+
+Phoenix will need to be run again.
+
+Also now is a good time to activate **Open at Login** on the Phoenix OS X
+menu item, if you like.
 
 ### Install CoffeeScript
 
@@ -41,10 +50,15 @@ I assume you know what you're doing, if not, I wish you luck on your
 diligent googling. (let's face it you got this far, you can get a
 couple of command line tools installed, right?)
 
+## Config begins here
+
+    Phoenix.notify "Phoenix config loading"
+
 ## Debugging helpers
 
-    debug = (message)->
-      api.alert message, 10
+    debug = (o, label="obj: ")->
+      Phoenix.log label
+      Phoenix.log JSON.stringify(o)
 
 ## Basic Settings
 
@@ -55,122 +69,109 @@ couple of command line tools installed, right?)
 
 ## Methods
 
+### Helpers
+
+    focused = -> Window.focusedWindow()
+    windows = -> Window.visibleWindows()
+    Window::screenRect = -> @screen().frameInRectangle()
+
 ### Window Grid
 
 Snap all windows to grid layout
 
     snapAllToGrid = ->
-      Window.visibleWindows().map (win) ->
-        win.snapToGrid()
-        return
-      return
+      _.map visible(), (win) -> win.snapToGrid()
 
-Change grid by a width factor
+Change grid width or height by factor
 
-    changeGridWidth = (by_) ->
-      GRID_WIDTH = Math.max(1, GRID_WIDTH + by_)
-      api.alert "grid is now " + GRID_WIDTH + " tiles wide", 1
+    changeGridWidth = (factor) ->
+      GRID_WIDTH = Math.max 1, GRID_WIDTH + factor
+      Phoenix.notify "grid is now " + GRID_WIDTH + " tiles wide"
       snapAllToGrid()
-      return
 
-    changeGridHeight = (by_) ->
-      GRID_HEIGHT = Math.max(1, GRID_HEIGHT + by_)
-      api.alert "grid is now " + GRID_HEIGHT + " tiles high", 1
+    changeGridHeight = (factor) ->
+      GRID_HEIGHT = Math.max 1, GRID_HEIGHT + factor
+      Phoenix.notify "grid is now " + GRID_HEIGHT + " tiles high"
       snapAllToGrid()
-      return
 
-Get the current grid as `{x:,y:,width:,height:}`
+Get the current grid as `{x:, y:, width:, height:}`
 
     Window::getGrid = ->
-      winFrame = @frame()
-      screenRect = @screen().frameWithoutDockOrMenu()
-      thirdScreenWidth = screenRect.width / GRID_WIDTH
-      halfScreenHeight = screenRect.height / GRID_HEIGHT
-      x: Math.round((winFrame.x - screenRect.x) / thirdScreenWidth)
-      y: Math.round((winFrame.y - screenRect.y) / halfScreenHeight)
-      width: Math.max(1, Math.round(winFrame.width / thirdScreenWidth))
-      height: Math.max(1, Math.round(winFrame.height / halfScreenHeight))
+      frame = @frame()
+      gridWidth = @screenRect().width / GRID_WIDTH
+      gridHeight = @screenRect().height / GRID_HEIGHT
 
-Set the current grid from an object `{x:,y:,width:,height:}`
+      x: Math.round (frame.x - @screenRect().x) / gridWidth
+      y: Math.round (frame.y - @screenRect().y) / gridHeight
+      width: Math.max 1, Math.round frame.width / gridWidth
+      height: Math.max 1, Math.round frame.height / gridHeight
+
+Set the current grid from an object `{x:, y:, width:, height:}`
 
     Window::setGrid = (grid, screen) ->
-      screenRect = screen.frameWithoutDockOrMenu()
-      thirdScreenWidth = screenRect.width / GRID_WIDTH
-      halfScreenHeight = screenRect.height / GRID_HEIGHT
-      newFrame =
-        x: (grid.x * thirdScreenWidth) + screenRect.x
-        y: (grid.y * halfScreenHeight) + screenRect.y
-        width: grid.width * thirdScreenWidth
-        height: grid.height * halfScreenHeight
-      newFrame.x += MARGIN_X
-      newFrame.y += MARGIN_Y
-      newFrame.width -= (MARGIN_X * 2.0)
-      newFrame.height -= (MARGIN_Y * 2.0)
-      @setFrame newFrame
+      gridWidth = @screenRect().width / GRID_WIDTH
+      gridHeight = @screenRect().height / GRID_HEIGHT
+
+      @setFrame
+        x: ((grid.x * gridWidth) + @screenRect().x) + MARGIN_X
+        y: ((grid.y * gridHeight) + @screenRect().y) + MARGIN_Y
+        width: (grid.width * gridWidth) - (MARGIN_X * 2.0)
+        height: (grid.height * gridHeight) - (MARGIN_Y * 2.0)
 
 Snap the current window to the grid
 
     Window::snapToGrid = ->
-      @setGrid @getGrid(), @screen()  if @isNormalWindow()
+      @setGrid @getGrid(), @screen() if @isNormal()
 
 Calculate the grid based on the parameters, `x`, `y`, `width`, `height`, (returning an object `{x:,y:,width:,height:}`)
 
     Window::calculateGrid = (x, y, width, height) ->
-      screen = @screen().frameWithoutDockOrMenu()
-      x: Math.round(x * screen.width) + MARGIN_X + screen.x
-      y: Math.round(y * screen.height) + MARGIN_Y + screen.y
-      width: Math.round(width * screen.width) - 2.0 * MARGIN_X
-      height: Math.round(height * screen.height) - 2.0 * MARGIN_Y
+      x: Math.round(x * @screenRect().width) + MARGIN_X + @screenRect().x
+      y: Math.round(y * @screenRect().height) + MARGIN_Y + @screenRect().y
+      width: Math.round(width * @screenRect().width) - 2.0 * MARGIN_X
+      height: Math.round(height * @screenRect().height) - 2.0 * MARGIN_Y
 
 Window to grid
 
     Window::toGrid = (x, y, width, height) ->
-      rect = @calculateGrid(x, y, width, height)
+      rect = @calculateGrid x, y, width, height
       @setFrame rect
-      this
+      @
 
 Window top right point
 
     Window::topRight = ->
-      f = @frame()
-      {
-        x: f.x + f.width
-        y: f.y
-      }
+      x: @frame().x + @frame().width
+      y: @frame().y
 
 Windows on the left
 
     Window::toLeft = ->
-      p = @topLeft()
       _.chain(@windowsToWest())
-      .filter (win)->
-        win.topLeft().x < p.x - 10
+      .filter (win)-> win.topLeft().x < @topLeft().x - 10
       .value()
 
 Windows on the right
 
     Window::toRight = ->
-      p = @topRight()
       _.chain(@windowsToEast())
-      .filter (win) ->
-        win.topRight().x > p.x + 10
+      .filter (win) -> win.topRight().x > @topRight().x + 10
       .value()
 
 ### Window information
 
     Window::info = ->
       f = @frame()
-      "[#{@app().pid}] #{@app().title()} : #{@title()}\n{x:#{f.x}, y:#{f.y}, width:#{f.width}, height:#{f.height}}\n"
+      "[#{@app().processIdentifier()}] #{@app().name()} : #{@title()}\n{x:#{f.x}, y:#{f.y}, width:#{f.width}, height:#{f.height}}\n"
 
 Sort any window collection by most recently with focus. We use
 `info()` as a way of identifying the windows in place. Not too
 performant, but with collections of this size, it's not a problem.
 
     Window.sortByMostRecent = (windows)->
-      allVisible = Window.visibleWindowsMostRecentFirst()
+      allVisible = visibleInOrder()
       _.chain(windows)
-      .sortBy (win)->
-        _.map(allVisible, (w)-> w.info()).indexOf(win.info())
+      .sortBy (win)-> _.map(allVisible, (w)-> w.info()).indexOf(win.info())
       .value()
 
 ### Window moving and sizing
@@ -197,123 +198,78 @@ Remember and forget frames
 
 Set a window to top / bottom / left / right
 
-    #                                      X     Y     Width  Height
-    Window::toTopHalf      =  ->  @toGrid  0,    0,    1,     0.5
-    Window::toBottomHalf   =  ->  @toGrid  0,    0.5,  1,     0.5
-    Window::toLeftHalf     =  ->  @toGrid  0,    0,    0.5,   1
-    Window::toRightHalf    =  ->  @toGrid  0.5,  0,    0.5,   1
-    #                                      X     Y     Width  Height
-    Window::toTopRight     =  ->  @toGrid  0.5,  0,    0.5,   0.5
-    Window::toBottomRight  =  ->  @toGrid  0.5,  0.5,  0.5,   0.5
-    Window::toTopLeft      =  ->  @toGrid  0,    0,    0.5,   0.5
-    Window::toBottomLeft   =  ->  @toGrid  0,    0.5,  0.5,   0.5
+    #                                  X    Y    Width Height
+    Window::toTopHalf     = -> @toGrid 0,   0,   1,    0.5
+    Window::toBottomHalf  = -> @toGrid 0,   0.5, 1,    0.5
+    Window::toLeftHalf    = -> @toGrid 0,   0,   0.5,  1
+    Window::toRightHalf   = -> @toGrid 0.5, 0,   0.5,  1
+    #                                  X    Y    Width Height
+    Window::toTopRight    = -> @toGrid 0.5, 0,   0.5,  0.5
+    Window::toBottomRight = -> @toGrid 0.5, 0.5, 0.5,  0.5
+    Window::toTopLeft     = -> @toGrid 0,   0,   0.5,  0.5
+    Window::toBottomLeft  = -> @toGrid 0,   0.5, 0.5,  0.5
 
 Move the current window to the next / previous screen
 
     moveWindowToNextScreen = ->
-      win = Window.focusedWindow()
-      win.setGrid win.getGrid(), win.screen().nextScreen()
+      focused().setGrid focused().getGrid(), focused().screen().nextScreen()
 
     moveWindowToPreviousScreen = ->
-      win = Window.focusedWindow()
-      win.setGrid win.getGrid(), win.screen().previousScreen()
+      focused().setGrid focused().getGrid(), focused().screen().previousScreen()
 
 Move the current window by one column
 
     moveWindowLeftOneColumn = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.x = Math.max(frame.x - 1, 0)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
     moveWindowRightOneColumn = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.x = Math.min(frame.x + 1, GRID_WIDTH - frame.width)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
 Grow and shrink the current window by a single grid cell
 
     windowGrowOneGridColumn = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.width = Math.min(frame.width + 1, GRID_WIDTH - frame.x)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
     windowShrinkOneGridColumn = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.width = Math.max(frame.width - 1, 1)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
     windowGrowOneGridRow = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.height = Math.min(frame.height + 1, GRID_HEIGHT)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
     windowShrinkOneGridRow = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.height = Math.max(frame.height - 1, 1)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
 Shift the current window to the bottom or top row
 
     windowDownOneRow = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.y = Math.min(Math.floor(frame.y + 1), GRID_HEIGHT - 1)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
     windowUpOneRow = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.y = Math.max(Math.floor(frame.y - 1), 0)
-      win.setGrid frame, win.screen()
+      focused().setGrid frame, focused().screen()
 
 Expand the current window's height to vertically fill the screen
 
     windowToFullHeight = ->
-      win = Window.focusedWindow()
-      frame = win.getGrid()
+      frame = focused().getGrid()
       frame.y = 0
       frame.height = GRID_HEIGHT
-      win.setGrid frame, win.screen()
-
-### Transpose windows
-
-This implementation is somewhat flawed, but this is by nature, due to
-it's rather uncommon use.  If it were more frequently executed, I'm
-sure it would be more fully formed, and sufficiently functional.
-Perhaps, also, I should read less Ambrose Bierce.
-
-    transposeWindows = (swapFrame = true, switchFocus = true)->
-      win = Window.focusedWindow()
-      left = win.toRight()
-      right = win.toLeft()
-      targets = if left.length > 0
-        left
-      else if right.length > 0
-        right
-
-      unless targets?.length > 0
-        api.alert "Can't see any windows to transpose"
-        return
-
-      target = Window.sortByMostRecent(targets)[0]
-
-      t_frame = target.frame()
-      w_frame = win.frame()
-
-      if swapFrame
-        win.setFrame t_frame
-        target.setFrame w_frame
-      else
-        target.topLeft x:w_frame.x, y:w_frame.y
-        win.topLeft    x:t_frame.x, y:t_frame.y
-
-      target.focusWindow() if switchFocus
+      focused().setGrid frame, focused().screen()
 
 ### Applications
 
@@ -324,73 +280,67 @@ Select the first window for an app
 Find an app by it's `title` - this is problematic when the App window
 has no title bar. Fair warning.
 
-    App.byTitle = (title) ->
-      apps = @runningApps()
-      i = 0
-      while i < apps.length
-        app = apps[i]
-        if app.title() is title
-          app.show()
-          return app
-        i++
-      return
+    App.byName = (name) ->
+      app = _.first(App.allWithName(name))
+      app.show()
+      app
 
 Find all apps with `title`
 
-    App.allWithTitle = (title) ->
-      _(@runningApps()).filter (app) ->
-        true  if app.title() is title
+    App.allWithName = (name) ->
+      _.filter App.runningApps(), (app) -> app.name() is name
 
 Focus or start an app with `title`
 
-    App.focusOrStart = (title) ->
-      apps = App.allWithTitle(title)
+    App.focusOrStart = (name) ->
+      apps = App.allWithName(name)
       if _.isEmpty(apps)
-        api.alert "Attempting to start #{title}"
-        api.launch title
-        return
+        Phoenix.notify "Starting #{name}"
+        App.launch name
+      else
+        Phoenix.notify "Switching to #{name}"
 
-      windows = _.chain(apps)
-      .map (x) ->
-        x.allWindows()
+      windows = _.chain apps
+      .map (x) -> x.windows()
       .flatten()
       .value()
 
       activeWindows = _(windows)
       .reject (win) ->
-        win.isWindowMinimized()
+        win.isMinimized()
 
       if _.isEmpty(activeWindows)
-        api.launch title
+        App.launch name
 
       activeWindows.forEach (win) ->
-        win.focusWindow()
-        return
-      return
+        win.focus()
 
-Run the given function `fn` for an app with `title`
+Run the given function `fn` for an app with `name`
 
-    forApp = (title, fn) ->
-      app = App.byTitle(title)
-      _.each app.visibleWindows(), fn  if app
+    forApp = (name, fn) ->
+      app = App.byName(name)
+      _.each app.visibleWindows(), fn if app
 
 ### Binding alias
 
-Alias `api.bind` as `key_binding`, to make the binding table extra
+Alias `Phoenix.bind` as `key_binding`, to make the binding table extra
 readable.
+
+    keys = []
 
     key_binding = (key, description, modifier, fn)->
       # Description is just to keep the key binding metadata in the
       # method call so we can easily build the keyboard guide without
       # additional metadata elsewhere.  It would also be helpful for
-      # doing things like a describe-key command.
-      api.bind key, modifier, fn
+      # doing things like a describe-key command. Of course, this is
+      # just speculative at the moment tbc...
+      keys.push Phoenix.bind(key, modifier, fn)
 
 ## Bindings
 
 Mash is <kbd>Cmd</kbd> + <kbd>Alt/Opt</kbd> + <kbd>Ctrl</kbd> pressed together.
 
-    mash = 'cmd+alt+ctrl'.split '+'
+    mash = 'cmd alt ctrl'.split ' '
 
 Move the current window to the top / bottom / left / right half of the screen
 and fill it.
@@ -442,8 +392,8 @@ Setting the grid size
 
 Snap current window or all windows to the grid
 
-    key_binding ';', 'Snap focussed to grid',      mash, -> Window.focusedWindow().snapToGrid()
-    key_binding "'", 'Snap all to grid',           mash, -> Window.visibleWindows().map (win)-> win.snapToGrid()
+    key_binding ';', 'Snap focused to grid',       mash, -> Window.focusedWindow().snapToGrid()
+    key_binding "'", 'Snap all to grid',           mash, -> visible().map (win)-> win.snapToGrid()
 
 Move the current window around the grid
 
@@ -460,5 +410,6 @@ Size the current window on the grid
     key_binding ',', 'Shrink by One Row',          mash, -> windowShrinkOneGridRow()
     key_binding '.', 'Grow by One Row',            mash, -> windowGrowOneGridRow()
 
-Note: `api.runCommand` is undocumented in the API ref, I've included
-the method signature in the API ref in this gist.
+The end...
+
+    Phoenix.notify "Loaded"
