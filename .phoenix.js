84798225abec129c595a68e64b5b2e8f48f38cae
jasonm23 <jasonm23@gmail.com>
2015-11-22 15:34:40 +0800

    Rework the coffeescript a little

--- .phoenix.js
+++ .phoenix.js
@@ -69,6 +69,12 @@ couple of command line tools installed, right?)
 
 ## Methods
 
+### Underscore extensions
+
+    _.mixin
+      flatmap: (list, iteratee, context) ->
+        _.flatten _.map list, iteratee, context
+
 ### Helpers
 
     focused = -> Window.focusedWindow()
@@ -82,16 +88,16 @@ Snap all windows to grid layout
     snapAllToGrid = ->
       _.map visible(), (win) -> win.snapToGrid()
 
-Change grid width or height by factor
+Change grid width or height
 
-    changeGridWidth = (factor) ->
-      GRID_WIDTH = Math.max 1, GRID_WIDTH + factor
-      Phoenix.notify "grid is now " + GRID_WIDTH + " tiles wide"
+    changeGridWidth = (n) ->
+      GRID_WIDTH = Math.max 1, GRID_WIDTH + n
+      Phoenix.notify "grid is #{GRID_WIDTH} tiles wide"
       snapAllToGrid()
 
-    changeGridHeight = (factor) ->
-      GRID_HEIGHT = Math.max 1, GRID_HEIGHT + factor
-      Phoenix.notify "grid is now " + GRID_HEIGHT + " tiles high"
+    changeGridHeight = (n) ->
+      GRID_HEIGHT = Math.max 1, GRID_HEIGHT + n
+      Phoenix.notify "grid is #{GRID_HEIGHT} tiles high"
       snapAllToGrid()
 
 Get the current grid as `{x:, y:, width:, height:}`
@@ -125,7 +131,7 @@ Snap the current window to the grid
 
 Calculate the grid based on the parameters, `x`, `y`, `width`, `height`, (returning an object `{x:,y:,width:,height:}`)
 
-    Window::calculateGrid = (x, y, width, height) ->
+    Window::calculateGrid = ({x, y, width, height}) ->
       x: Math.round(x * @screenRect().width) + MARGIN_X + @screenRect().x
       y: Math.round(y * @screenRect().height) + MARGIN_Y + @screenRect().y
       width: Math.round(width * @screenRect().width) - 2.0 * MARGIN_X
@@ -133,8 +139,8 @@ Calculate the grid based on the parameters, `x`, `y`, `width`, `height`, (return
 
 Window to grid
 
-    Window::toGrid = (x, y, width, height) ->
-      rect = @calculateGrid x, y, width, height
+    Window::toGrid = ({x, y, width, height}) ->
+      rect = @calculateGrid {x, y, width, height}
       @setFrame rect
       @
 
@@ -147,16 +153,14 @@ Window top right point
 Windows on the left
 
     Window::toLeft = ->
-      _.chain(@windowsToWest())
-      .filter (win)-> win.topLeft().x < @topLeft().x - 10
-      .value()
+      _.filter @windowsToWest(), (win)->
+        win.topLeft().x < @topLeft().x - 10
 
 Windows on the right
 
     Window::toRight = ->
-      _.chain(@windowsToEast())
-      .filter (win) -> win.topRight().x > @topRight().x + 10
-      .value()
+      _.filter @windowsToEast(), (win) ->
+        win.topRight().x > @topRight().x + 10
 
 ### Window information
 
@@ -170,9 +174,9 @@ performant, but with collections of this size, it's not a problem.
 
     Window.sortByMostRecent = (windows)->
       allVisible = visibleInOrder()
-      _.chain(windows)
-      .sortBy (win)-> _.map(allVisible, (w)-> w.info()).indexOf(win.info())
-      .value()
+      _.sortBy windows, (win)->
+        _.map(allVisible, (w)->
+          w.info()).indexOf win.info()
 
 ### Window moving and sizing
 
@@ -183,8 +187,8 @@ Temporary storage for frames
 Set a window to full screen
 
     Window::toFullScreen = ->
-      fullFrame = @calculateGrid(0, 0, 1, 1)
-      unless _.isEqual(@frame(), fullFrame)
+      fullFrame = @calculateGrid 0, 0, 1, 1
+      unless _.isEqual @frame(), fullFrame
         @rememberFrame()
         @toGrid 0, 0, 1, 1
       else if lastFrames[this]
@@ -198,16 +202,15 @@ Remember and forget frames
 
 Set a window to top / bottom / left / right
 
-    #                                  X    Y    Width Height
-    Window::toTopHalf     = -> @toGrid 0,   0,   1,    0.5
-    Window::toBottomHalf  = -> @toGrid 0,   0.5, 1,    0.5
-    Window::toLeftHalf    = -> @toGrid 0,   0,   0.5,  1
-    Window::toRightHalf   = -> @toGrid 0.5, 0,   0.5,  1
-    #                                  X    Y    Width Height
-    Window::toTopRight    = -> @toGrid 0.5, 0,   0.5,  0.5
-    Window::toBottomRight = -> @toGrid 0.5, 0.5, 0.5,  0.5
-    Window::toTopLeft     = -> @toGrid 0,   0,   0.5,  0.5
-    Window::toBottomLeft  = -> @toGrid 0,   0.5, 0.5,  0.5
+    Window::toTopHalf     = -> @toGrid x:0,   y:0,   width:1,    height:0.5
+    Window::toBottomHalf  = -> @toGrid x:0,   y:0.5, width:1,    height:0.5
+    Window::toLeftHalf    = -> @toGrid x:0,   y:0,   width:0.5,  height:1
+    Window::toRightHalf   = -> @toGrid x:0.5, y:0,   width:0.5,  height:1
+
+    Window::toTopRight    = -> @toGrid x:0.5, y:0,   width:0.5,  height:0.5
+    Window::toBottomRight = -> @toGrid x:0.5, y:0.5, width:0.5,  height:0.5
+    Window::toTopLeft     = -> @toGrid x:0,   y:0,   width:0.5,  height:0.5
+    Window::toBottomLeft  = -> @toGrid x:0,   y:0.5, width:0.5,  height:0.5
 
 Move the current window to the next / previous screen
 
@@ -217,19 +220,29 @@ Move the current window to the next / previous screen
     moveWindowToPreviousScreen = ->
       focused().setGrid focused().getGrid(), focused().screen().previousScreen()
 
-Move the current window by one column
+Move the current window around the grid
 
-    moveWindowLeftOneColumn = ->
+    windowLeftOneColumn = ->
       frame = focused().getGrid()
       frame.x = Math.max(frame.x - 1, 0)
       focused().setGrid frame, focused().screen()
 
-    moveWindowRightOneColumn = ->
+    windowDownOneRow = ->
+      frame = focused().getGrid()
+      frame.y = Math.min Math.floor(frame.y + 1), GRID_HEIGHT - 1
+      focused().setGrid frame, focused().screen()
+
+    windowUpOneRow = ->
+      frame = focused().getGrid()
+      frame.y = Math.max Math.floor(frame.y - 1), 0
+      focused().setGrid frame, focused().screen()
+
+    windowRightOneColumn = ->
       frame = focused().getGrid()
       frame.x = Math.min(frame.x + 1, GRID_WIDTH - frame.width)
       focused().setGrid frame, focused().screen()
 
-Grow and shrink the current window by a single grid cell
+Resize the current window on the grid
 
     windowGrowOneGridColumn = ->
       frame = focused().getGrid()
@@ -243,24 +256,12 @@ Grow and shrink the current window by a single grid cell
 
     windowGrowOneGridRow = ->
       frame = focused().getGrid()
-      frame.height = Math.min(frame.height + 1, GRID_HEIGHT)
+      frame.height = Math.min frame.height + 1, GRID_HEIGHT
       focused().setGrid frame, focused().screen()
 
     windowShrinkOneGridRow = ->
       frame = focused().getGrid()
-      frame.height = Math.max(frame.height - 1, 1)
-      focused().setGrid frame, focused().screen()
-
-Shift the current window to the bottom or top row
-
-    windowDownOneRow = ->
-      frame = focused().getGrid()
-      frame.y = Math.min(Math.floor(frame.y + 1), GRID_HEIGHT - 1)
-      focused().setGrid frame, focused().screen()
-
-    windowUpOneRow = ->
-      frame = focused().getGrid()
-      frame.y = Math.max(Math.floor(frame.y - 1), 0)
+      frame.height = Math.max frame.height - 1, 1
       focused().setGrid frame, focused().screen()
 
 Expand the current window's height to vertically fill the screen
@@ -277,49 +278,37 @@ Select the first window for an app
 
     App::firstWindow = -> @visibleWindows()[0]
 
-Find an app by it's `title` - this is problematic when the App window
+Find an app by it's `name` - this is problematic when the App window
 has no title bar. Fair warning.
 
-    App.byName = (name) ->
-      app = _.first(App.allWithName(name))
-      app.show()
-      app
-
-Find all apps with `title`
+Find all apps with `name`
 
     App.allWithName = (name) ->
       _.filter App.runningApps(), (app) -> app.name() is name
 
-Focus or start an app with `title`
+    App.byName = (name) ->
+      app = _.first App.allWithName name
+      app.show()
+      app
+
+Focus or start an app with `name`
 
     App.focusOrStart = (name) ->
-      apps = App.allWithName(name)
-      if _.isEmpty(apps)
+      apps = App.allWithName name
+      if _.isEmpty apps
         Phoenix.notify "Starting #{name}"
         App.launch name
       else
         Phoenix.notify "Switching to #{name}"
 
-      windows = _.chain apps
-      .map (x) -> x.windows()
-      .flatten()
-      .value()
+      windows = _.flatmap apps, (x) -> x.windows()
 
-      activeWindows = _(windows)
-      .reject (win) ->
-        win.isMinimized()
+      activeWindows = _.reject windows, (win) -> win.isMinimized()
 
       if _.isEmpty(activeWindows)
         App.launch name
 
-      activeWindows.forEach (win) ->
-        win.focus()
-
-Run the given function `fn` for an app with `name`
-
-    forApp = (name, fn) ->
-      app = App.byName(name)
-      _.each app.visibleWindows(), fn if app
+      activeWindows.forEach (win) -> win.focus()
 
 ### Binding alias
 
@@ -328,38 +317,36 @@ readable.
 
     keys = []
 
+The `key_binding` method includes the unused `description` parameter,
+This is to allow future functionality ie. help mechanisms, describe bindings etc.
+
     key_binding = (key, description, modifier, fn)->
-      # Description is just to keep the key binding metadata in the
-      # method call so we can easily build the keyboard guide without
-      # additional metadata elsewhere.  It would also be helpful for
-      # doing things like a describe-key command. Of course, this is
-      # just speculative at the moment tbc...
       keys.push Phoenix.bind(key, modifier, fn)
 
 ## Bindings
 
 Mash is <kbd>Cmd</kbd> + <kbd>Alt/Opt</kbd> + <kbd>Ctrl</kbd> pressed together.
 
-    mash = 'cmd alt ctrl'.split ' '
+    mash = 'cmd-alt-ctrl'.split '-'
 
 Move the current window to the top / bottom / left / right half of the screen
 and fill it.
 
-    key_binding 'up',    'To Top Half',            mash, -> Window.focusedWindow().toTopHalf()
-    key_binding 'down',  'To Bottom Half',         mash, -> Window.focusedWindow().toBottomHalf()
-    key_binding 'left',  'To Left Half',           mash, -> Window.focusedWindow().toLeftHalf()
-    key_binding 'right', 'To Right Half',          mash, -> Window.focusedWindow().toRightHalf()
+    key_binding 'up',    'To Top Half',         mash, -> Window.focusedWindow().toTopHalf()
+    key_binding 'down',  'To Bottom Half',      mash, -> Window.focusedWindow().toBottomHalf()
+    key_binding 'left',  'To Left Half',        mash, -> Window.focusedWindow().toLeftHalf()
+    key_binding 'right', 'To Right Half',       mash, -> Window.focusedWindow().toRightHalf()
 
 Move to the corners of the screen
 
-    key_binding 'Q', 'Top Left',                   mash, -> Window.focusedWindow().toTopLeft()
-    key_binding 'A', 'Bottom Left',                mash, -> Window.focusedWindow().toBottomLeft()
-    key_binding 'W', 'Top Right',                  mash, -> Window.focusedWindow().toTopRight()
-    key_binding 'S', 'Bottom Right',               mash, -> Window.focusedWindow().toBottomRight()
+    key_binding 'Q', 'Top Left',                mash, -> Window.focusedWindow().toTopLeft()
+    key_binding 'A', 'Bottom Left',             mash, -> Window.focusedWindow().toBottomLeft()
+    key_binding 'W', 'Top Right',               mash, -> Window.focusedWindow().toTopRight()
+    key_binding 'S', 'Bottom Right',            mash, -> Window.focusedWindow().toBottomRight()
 
 Toggle maximize for the current window
 
-    key_binding 'space', 'Maximize Window',        mash, -> Window.focusedWindow().toFullScreen()
+    key_binding 'space', 'Maximize Window',     mash, -> Window.focusedWindow().toFullScreen()
 
 ## Application config
 
@@ -372,43 +359,43 @@ Toggle maximize for the current window
 
 Switch to or lauch apps
 
-    key_binding 'E', 'Launch Emacs',               mash, -> App.focusOrStart EMACS
-    key_binding 'V', 'Launch Vim',                 mash, -> App.focusOrStart VIM
-    key_binding 'T', 'Launch iTerm2',              mash, -> App.focusOrStart ITERM
-    key_binding 'C', 'Launch Chrome',              mash, -> App.focusOrStart CHROME
-    key_binding 'F', 'Launch Finder',              mash, -> App.focusOrStart FINDER
+    key_binding 'E', 'Launch Emacs',            mash, -> App.focusOrStart EMACS
+    key_binding 'V', 'Launch Vim',              mash, -> App.focusOrStart VIM
+    key_binding 'T', 'Launch iTerm2',           mash, -> App.focusOrStart ITERM
+    key_binding 'C', 'Launch Chrome',           mash, -> App.focusOrStart CHROME
+    key_binding 'F', 'Launch Finder',           mash, -> App.focusOrStart FINDER
 
 Move window between screens
 
-    key_binding 'N', 'To Next Screen',             mash, -> moveWindowToNextScreen()
-    key_binding 'P', 'To Previous Screen',         mash, -> moveWindowToPreviousScreen()
+    key_binding 'N', 'To Next Screen',          mash, -> moveWindowToNextScreen()
+    key_binding 'P', 'To Previous Screen',      mash, -> moveWindowToPreviousScreen()
 
 Setting the grid size
 
-    key_binding '=', 'Increase Grid Columns',      mash, -> changeGridWidth +1
-    key_binding '-', 'Reduce Grid Columns',        mash, -> changeGridWidth -1
-    key_binding '[', 'Increase Grid Rows',         mash, -> changeGridHeight +1
-    key_binding ']', 'Reduce Grid Rows',           mash, -> changeGridHeight -1
+    key_binding '=', 'Increase Grid Columns',   mash, -> changeGridWidth +1
+    key_binding '-', 'Reduce Grid Columns',     mash, -> changeGridWidth -1
+    key_binding '[', 'Increase Grid Rows',      mash, -> changeGridHeight +1
+    key_binding ']', 'Reduce Grid Rows',        mash, -> changeGridHeight -1
 
 Snap current window or all windows to the grid
 
-    key_binding ';', 'Snap focused to grid',       mash, -> Window.focusedWindow().snapToGrid()
-    key_binding "'", 'Snap all to grid',           mash, -> visible().map (win)-> win.snapToGrid()
+    key_binding ';', 'Snap focused to grid',    mash, -> Window.focusedWindow().snapToGrid()
+    key_binding "'", 'Snap all to grid',        mash, -> visible().map (win)-> win.snapToGrid()
 
 Move the current window around the grid
 
-    key_binding 'H', 'Move Grid Left',             mash, -> moveWindowLeftOneColumn()
-    key_binding 'J', 'Move Grid Down',             mash, -> windowDownOneRow()
-    key_binding 'K', 'Move Grid Up',               mash, -> windowUpOneRow()
-    key_binding 'L', 'Move Grid Right',            mash, -> moveWindowRightOneColumn()
+    key_binding 'H', 'Move Grid Left',          mash, -> windowLeftOneColumn()
+    key_binding 'J', 'Move Grid Down',          mash, -> windowDownOneRow()
+    key_binding 'K', 'Move Grid Up',            mash, -> windowUpOneRow()
+    key_binding 'L', 'Move Grid Right',         mash, -> windowRightOneColumn()
 
 Size the current window on the grid
 
-    key_binding 'U', 'Window Full Height',         mash, -> windowToFullHeight()
-    key_binding 'I', 'Shrink by One Column',       mash, -> windowShrinkOneGridColumn()
-    key_binding 'O', 'Grow by One Column',         mash, -> windowGrowOneGridColumn()
-    key_binding ',', 'Shrink by One Row',          mash, -> windowShrinkOneGridRow()
-    key_binding '.', 'Grow by One Row',            mash, -> windowGrowOneGridRow()
+    key_binding 'U', 'Window Full Height',      mash, -> windowToFullHeight()
+    key_binding 'I', 'Shrink by One Column',    mash, -> windowShrinkOneGridColumn()
+    key_binding 'O', 'Grow by One Column',      mash, -> windowGrowOneGridColumn()
+    key_binding ',', 'Shrink by One Row',       mash, -> windowShrinkOneGridRow()
+    key_binding '.', 'Grow by One Row',         mash, -> windowGrowOneGridRow()
 
 The end...
 
